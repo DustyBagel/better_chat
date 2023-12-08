bcd4d7af5a7dad62c76378feaf63ddd64876b8df
DustyBagel <150477174+DustyBagel@users.noreply.github.com>
2023-12-08 07:11:33 -0600

    Add more date formats

--- init.lua
+++ init.lua
@@ -1,7 +1,3 @@
--- Get the settings
-local time_format = minetest.settings:get("time_format") --or "24_hour"
-local date_format = minetest.settings:get("date_format") --or "dd-mm-yyyy"
-
 -- Convert the settings to the corresponding format strings
 if time_format == "24_hour" then 
 	time_format = "%H:%M:%S" 
@@ -9,10 +5,24 @@ else
 	time_format = "%I:%M:%S %p" 
 end
 
-if date_format == "mm-dd-yyyy" then 
-	date_format = "%m/%d/%Y" 
+if date_separator == "slash" then 
+	date_separator_char = "/"
+elseif date_separator == "dash" then 
+	date_separator_char = "-"
+elseif date_separator == "period" then 
+	date_separator_char = "."
+elseif date_separator == "comma" then 
+	date_separator_char = ","
+end
+
+if date_order == "month_day_year" then 
+	date_format = "%m"..date_separator_char.."%d"..date_separator_char.."%Y" 
+elseif date_order == "year_month_day" then 
+	date_format = "%Y"..date_separator_char.."%m"..date_separator_char.."%d" 
+elseif date_order == "year_day_month" then 
+	date_format = "%Y"..date_separator_char.."%d"..date_separator_char.."%m" 
 else 
-	date_format = "%d/%m/%Y" 
+	date_format = "%d"..date_separator_char.."%m"..date_separator_char.."%Y" 
 end
 
 -- Modify the chat message
